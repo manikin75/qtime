873e229940ec05b2849b7c497f9aed1dc7434382
Martin Lundberg <martin.lundberg@qvantum.com>
2026-02-25 16:40:15 +0100

    fix: removed console log

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,6 @@ import './dist.css';
 const queryClient = new QueryClient();
 
 function App() {
-  console.log(import.meta.env.VITE_CLIENT_ID);
-
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
